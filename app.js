5baadca1dd2143f84686c660d1fcb24bf3963348
WouterButhker <wouterbuthker@live.nl>
2020-01-14 00:41:18 +0100

    Routing toegevoegd

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require("express");
 var http = require("http");
+var indexRouter = require("./routes/index");
 
 var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
+
+app.get("/", indexRouter);
+app.get("/play", indexRouter);
+
 http.createServer(app).listen(port);
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+
+// splash screen
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  //res.render('index', { title: 'Express' });
+  res.sendFile("splash.html", {root: "./public"})
 });
+
+// game screen
+router.get('/play', function (req, res) {
+  res.sendFile("game.html", {root: "./public"})
+});
+
+
 
 module.exports = router;
