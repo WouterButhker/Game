70c4149e60797fb275fb584be7cf1650302438bc
WouterButhker <wouterbuthker@live.nl>
2020-01-15 18:12:58 +0100

    Alles is anders

--- app.js
+++ app.js
@@ -7,25 +7,123 @@ let app = express();
 require("./routes/index")(app);
 app.use(express.static(__dirname + "/public"));
 
-ficheArray = [];
-for (let i = 0; i < 7; i++) {
-    ficheArray[i] = [];
-}
+// ficheArray = [];
+// for (let i = 0; i < 7; i++) {
+//     ficheArray[i] = [];
+// }
+//
+//
+
+// TODO meer games tegelijk fixen
+var currentGames = [];
+var gameCounter = 0;
 
 
+var gameObj = {
+    player1: null,
+    player2: null,
+    gameNumber: 0,
+    turn: "red",
+    board: [],
+    init: function() {
+        this.board = [];
+        for (let i = 0; i < 7; i++) {
+            this.board[i] = [];
+        }
+    },
+    fichesPlayed: 0,
+    addPlayer: function(ws) {
+        if (this.player1 == null) {
+            this.player1 = ws;
+            return true;
+        } else if (this.player2 == null) {
+            this.player2 = ws;
+            return true
+        } else {
+            return false;
+        }
+    },
+    bothPlayersPresent: function () {
+        return this.player1 != null && this.player2 != null;
+    },
+    sendToPlayers: function (message) {
+        if (this.bothPlayersPresent()) {
+            this.player1.send(message);
+            this.player2.send(message);
+            return true;
+        } else {
+            return false;
+        }
+    },
+    drop: function (player, column) {
+        //let col = ;
 
-let server = http.createServer(app).listen(port);
+        console.log(this.board);
+
+        for (let i = 0; i < 6; i++) {
+            if (this.board[column][i] === undefined) {
+                this.board[column][i] = player.color;
+
+                if (this.turn === "red") this.turn = "yellow";
+                else this.turn = "red";
+                return true;
+            }
+        }
+        return false;
+    }
+};
+
+//
+// var statistics = {
+//     numberOfPlayers: 0,
+//     totalGamesPlayed: 0,
+//     fastestGamePlayed: "none"
+// };
+
+
+let server = http.createServer(app);
 const socket = new websocket.Server({server});
 
 socket.on("connection", function (ws) {
-    console.log("Connection state: " + ws.readyState);
-    ws.send("Thanks for the message. --Your server.");
-    console.log("Connection state: " + ws.readyState);
+
+    let bool =  gameObj.addPlayer(ws);
+    //ws.send("Player connected \nSucces: " + bool);
+
+    if (gameObj.bothPlayersPresent()) {
+        //gameObj.sendToPlayers("LETS GO!!!");
+        gameObj.init();
+        console.log("Server: LETS GO!!!");
+        currentGames[gameCounter] = gameObj;
+        gameObj.player1.color = "red";
+        gameObj.player2.color = "yellow";
+    }
 
     ws.on("message", function incoming(message) {
-        message = JSON.parse(message);
-        console.log("[LOG] " + message.color);
+        //message = JSON.parse(message);
+        //console.log("[LOG] " + message);
+        let isPlayer1 = gameObj.player1 === ws;
+        if (isPlayer1) console.log("Player 1: Put some shit in " + message);
+        else console.log("Player 2: Put some shit in " + message);
+
+        let color = isPlayer1 ? gameObj.player1.color : gameObj.player2.color;
+        let player = isPlayer1 ? gameObj.player1 : gameObj.player2;
+        let success = false;
+        if (gameObj.turn === color) {
+            success = gameObj.drop(player, message);
+        }
+        //if (!success) player.send("Server: Move refused");
+
+        if (success) {
+
+        }
+        console.table(gameObj.board);
+
+        gameObj.sendToPlayers(JSON.stringify(gameObj.board));
+
     });
 });
+
+
+server.listen(port);
 
 
--- public/javascripts/game.js
+++ public/javascripts/game.js
@@ -1,12 +1,33 @@
-let elapsedTime = 0;
-let numberOfFiches = 0;
-let timerID;
-let finished = false;
+let socket = new WebSocket("ws://localhost:3000");
 
-let ficheArray = [];
-for (let i = 0; i < 7; i++) {
-    ficheArray[i] = [];
-}
+socket.onopen = function() {
+    console.log("Successfully connected to server")
+};
+
+socket.onmessage = function(event) {
+    let board = JSON.parse(event.data)
+    console.table(board);
+
+    for (let y = 0; y < 7; y++) {
+        for (let x = 0; x < 6; x++) {
+            if (board[x][y] !== undefined) {
+                document.getElementById(x + "" + y).classList.add(board[x][y]);
+            }
+        }
+    }
+};
+
+
+// let elapsedTime = 0;
+// let numberOfFiches = 0;
+// let timerID;
+// let finished = false;
+//
+// let ficheArray = [];
+// for (let i = 0; i < 7; i++) {
+//     ficheArray[i] = [];
+// }
+
 
 // create board
 for (let i = 41; i >= 0; i--) {
@@ -23,148 +44,149 @@ for (let i = 0; i < 7; i++) {
     column.id = i.toString();
     document.getElementById("columns").appendChild(column);
     column.addEventListener("click", function () {
-        dropFiche(i)
+        socket.send(i + "");
+        console.log("Client: Put some shit in " + i);
     });
 }
 
-timer();
-timerID = setInterval(timer, 1000);
-console.log("timer set!");
-
-
-function timer() {
-    let turnTimer = 30 - (elapsedTime % 30);
-    let elapsedMinutes = Math.floor(elapsedTime / 60);
-    let elapsedSeconds = elapsedTime % 60;
-
-    let optionalTimerZero = "";
-    if (elapsedSeconds < 10) {
-        optionalTimerZero = "0";
-    }
-
-    if (elapsedTime % 30 === 0) {
-        updateTurn();
-    }
-
-    let optionalTurnZero = "";
-    if (turnTimer < 10) {
-        optionalTurnZero = "0";
-    }
-
-    document.getElementById("elapsed").innerHTML = "Time elapsed: " + elapsedMinutes + ":" + optionalTimerZero + elapsedSeconds;
-    document.getElementById("turnTimer").innerHTML = "Turn timer: 0:" + optionalTurnZero + turnTimer;
-
-    if (numberOfFiches >= 42) {
-        finish();
-    }
-    elapsedTime ++;
-}
-
-function updateTurn() {
-    if (scanH(true) || scanH(false))
-        return;
-    let turnID = document.getElementById("turnID").innerHTML;
-    if (turnID === "RED")
-        document.getElementById("turnID").innerHTML = "YELLOW";
-    if (turnID === "YELLOW")
-        document.getElementById("turnID").innerHTML = "RED";
-    turnTimer = 30;
-}
-
-function dropFiche(y) {
-    if (y < 0 || y > 6 || finished)
-        return false;
-    for (let x = 0; x < 6; x++) {
-        let classList = document.getElementById(x + "" + y).classList;
-        let isRed = classList.contains("red");
-        let isYellow = classList.contains("yellow");
-        let turnID = document.getElementById("turnID").innerHTML;
-
-        if (!(isRed || isYellow)) {
-            classList.add(turnID.toLowerCase());
-            ficheArray[x][y] = turnID.toLowerCase();
-            numberOfFiches++;
-            updateTurn();
-            return true;
-        }
-    }
-    return false;
-}
-
-function finish() {
-    document.getElementById("finished").innerHTML = "yes";
-    console.log("FINISHED!!");
-    finished = true;
-    clearInterval(timerID);
-}
-
-function scanH(horizontal) {
-    // TODO: refactor better
-    // wtf is a b c d
-    // kan beter
-    let c, d;
-    if (horizontal) {
-        c = 6;
-        d = 7;
-    } else {
-        c = 7;
-        d = 6;
-    }
-
-    for (let a = 0; a < c; a++) {
-        let lastColor = null;
-        let consecutive = 1;
-        for (let b = 0; b < d; b++) {
-            let currentColor;
-            if (horizontal) currentColor = ficheArray[a][b];
-            else currentColor = ficheArray[b][a];
-
-            if (currentColor === lastColor && currentColor != null)
-                consecutive++;
-            else
-                consecutive = 1;
-            lastColor = currentColor;
-            if (consecutive === 4) {
-                document.getElementById("winner").innerHTML = "The winner is " + currentColor;
-                console.log("The winner is " + currentColor);
-                // TODO
-                console.log(a + ", " + b);
-                finish();
-                return true;
-            }
-        }
-    }
-    return false;
-}
-
-function logColors() {
-    for (let x = 0; x < 7; x++) {
-        for (let y = 0; y < 6; y++) {
-            let currentColor = ficheArray[x][y];
-            console.log("Array indexes: " + x + ", " + y);
-            console.log(currentColor);
-        }
-    }
-}
-
-function kill() {
-    clearInterval(timerID);
-}
-
-function sendMove(move) {
-    let socket = new WebSocket("ws://localhost:3000");
-    socket.onmessage = function (event) {
-        console.log(event.data);
-    }
-    socket.onopen = function () {
-        socket.send(JSON.stringify(move));
-    };
-}
-
-function colorFiche(move)
-{
-
-}
+// timer();
+// timerID = setInterval(timer, 1000);
+// console.log("timer set!");
+//
+//
+// function timer() {
+//     let turnTimer = 30 - (elapsedTime % 30);
+//     let elapsedMinutes = Math.floor(elapsedTime / 60);
+//     let elapsedSeconds = elapsedTime % 60;
+//
+//     let optionalTimerZero = "";
+//     if (elapsedSeconds < 10) {
+//         optionalTimerZero = "0";
+//     }
+//
+//     if (elapsedTime % 30 === 0) {
+//         updateTurn();
+//     }
+//
+//     let optionalTurnZero = "";
+//     if (turnTimer < 10) {
+//         optionalTurnZero = "0";
+//     }
+//
+//     document.getElementById("elapsed").innerHTML = "Time elapsed: " + elapsedMinutes + ":" + optionalTimerZero + elapsedSeconds;
+//     document.getElementById("turnTimer").innerHTML = "Turn timer: 0:" + optionalTurnZero + turnTimer;
+//
+//     if (numberOfFiches >= 42) {
+//         finish();
+//     }
+//     elapsedTime ++;
+// }
+//
+// function updateTurn() {
+//     if (scanH(true) || scanH(false))
+//         return;
+//     let turnID = document.getElementById("turnID").innerHTML;
+//     if (turnID === "RED")
+//         document.getElementById("turnID").innerHTML = "YELLOW";
+//     if (turnID === "YELLOW")
+//         document.getElementById("turnID").innerHTML = "RED";
+//     turnTimer = 30;
+// }
+//
+// function dropFiche(y) {
+//     if (y < 0 || y > 6 || finished)
+//         return false;
+//     for (let x = 0; x < 6; x++) {
+//         let classList = document.getElementById(x + "" + y).classList;
+//         let isRed = classList.contains("red");
+//         let isYellow = classList.contains("yellow");
+//         let turnID = document.getElementById("turnID").innerHTML;
+//
+//         if (!(isRed || isYellow)) {
+//             classList.add(turnID.toLowerCase());
+//             ficheArray[x][y] = turnID.toLowerCase();
+//             numberOfFiches++;
+//             updateTurn();
+//             return true;
+//         }
+//     }
+//     return false;
+// }
+//
+// function finish() {
+//     document.getElementById("finished").innerHTML = "yes";
+//     console.log("FINISHED!!");
+//     finished = true;
+//     clearInterval(timerID);
+// }
+//
+// function scanH(horizontal) {
+//     // TODO: refactor better
+//     // wtf is a b c d
+//     // kan beter
+//     let c, d;
+//     if (horizontal) {
+//         c = 6;
+//         d = 7;
+//     } else {
+//         c = 7;
+//         d = 6;
+//     }
+//
+//     for (let a = 0; a < c; a++) {
+//         let lastColor = null;
+//         let consecutive = 1;
+//         for (let b = 0; b < d; b++) {
+//             let currentColor;
+//             if (horizontal) currentColor = ficheArray[a][b];
+//             else currentColor = ficheArray[b][a];
+//
+//             if (currentColor === lastColor && currentColor != null)
+//                 consecutive++;
+//             else
+//                 consecutive = 1;
+//             lastColor = currentColor;
+//             if (consecutive === 4) {
+//                 document.getElementById("winner").innerHTML = "The winner is " + currentColor;
+//                 console.log("The winner is " + currentColor);
+//                 // TODO
+//                 console.log(a + ", " + b);
+//                 finish();
+//                 return true;
+//             }
+//         }
+//     }
+//     return false;
+// }
+//
+// function logColors() {
+//     for (let x = 0; x < 7; x++) {
+//         for (let y = 0; y < 6; y++) {
+//             let currentColor = ficheArray[x][y];
+//             console.log("Array indexes: " + x + ", " + y);
+//             console.log(currentColor);
+//         }
+//     }
+// }
+//
+// function kill() {
+//     clearInterval(timerID);
+// }
+//
+// function sendMove(move) {
+//     let socket = new WebSocket("ws://localhost:3000");
+//     socket.onmessage = function (event) {
+//         console.log(event.data);
+//     }
+//     socket.onopen = function () {
+//         socket.send(JSON.stringify(move));
+//     };
+// }
+//
+// function colorFiche(move)
+// {
+//
+// }
 
 
 
