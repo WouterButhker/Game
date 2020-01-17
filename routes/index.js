100a8e506020c5509175856162ac1e3f52cded50
WouterButhker <wouterbuthker@live.nl>
2020-01-17 16:43:32 +0100

    Ik weet niet hoe cookies werken

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,19 @@ var router = express.Router();
 
 module.exports = function(app) {
 
+  // app.use(function (req, res, next) {
+  //     let cookie = req.cookies;
+  //     if (cookie === undefined) {
+  //       let num = Math.random().toString();
+  //       num = num.substring(2, num.length);
+  //       res.cookie("cookieName", num);
+  //       console.log("created cookie")
+  //     } else {
+  //       console.log("Welcome back")
+  //     }
+  //     next();
+  // });
+
 
   app.get('/', function (req, res, next) {
     //res.render('index', { title: 'Express' });
